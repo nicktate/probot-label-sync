14822c1c533930435354b87c5bb065c70d56cbd7
nicktate <ntate22@gmail.com>
2019-03-19 13:44:59 -0400

    added label syncing logic

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const _ = require('lodash')
+const yaml = require('js-yaml')
 
-const PREVIEW_HEADER = 'application/vnd.github.machine-man-preview+json'
+const Github = require('./lib/github')
+
 const REPO_OR_LABEL_DIR_REGEX = /^\/(labels|repositories)/
+
+const buildLabelArray = (files) => {
+  const labels = _.map(files, f => {
+    return f ? yaml.safeLoad(f.content).labels : []
+  })
+
+  return _.flatten(labels)
+}
 
 module.exports = (app, Repository = require('./lib/repository')) => {
   app.on('push', async context => {
@@ -15,18 +25,23 @@ module.exports = (app, Repository = require('./lib/repository')) => {
       ], c => REPO_OR_LABEL_DIR_REGEX.test(c))
     })
 
-    //console.log(JSON.stringify(payload, null, 2))
-    if(true || defaultBranch && labelsOrReposModified) {
-      const result = await context.github.repos.getContents({
-        owner: context.repo().owner,
-        repo: context.repo().repo,
-        path: 'README.md',
-        headers: {
-          accept: PREVIEW_HEADER
-        }
-      })
+    if (defaultBranch && labelsOrReposModified) {
+      const labels = _.keyBy(await Github.getFilesRecursively(context.github, context.repo(), 'labels'), 'path')
+      const repos = await Github.getFilesRecursively(context.github, context.repo(), 'repositories')
 
-      console.log(result)
+      for (const r of repos) {
+        // formatted /repositories/containership/example
+        // eslint-disable-next-line no-unused-vars
+        let [_ignore, org, name] = r.path.split('/')
+        // strip file extension
+        name = name.substring(0, name.indexOf('.yaml'))
+
+        const labelPaths = _.map(yaml.safeLoad(r.content).labelGroups, l => `labels/${l}`)
+        const repoLabels = buildLabelArray(_.values(_.pick(labels, labelPaths)))
+
+        console.log(`Syncing labels for ${org}/${name}...`)
+        await Repository.syncLabels(context.github, { owner: org, repo: name }, repoLabels)
+      }
     }
   })
 }
